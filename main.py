7f9c8bb7aeb4eb4ac1a7c29013f13603f66e51d9
David Yrigoyen <dyrigoyen@tyr.com>
2023-04-19 16:30:00 -0700

    fixed the selenium deprecation error

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import connect
 
-driver = webdriver.Chrome(executable_path=connect.CHROME_DRIVER_PATH)
+chrome_driver_path = Service(connect.CHROME_DRIVER_PATH)
+driver = webdriver.Chrome(service=chrome_driver_path)
+
+driver.get("https://www.amazon.com")
